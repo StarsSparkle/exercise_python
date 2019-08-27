7d091e610b1b87936b53a1fb97b147f5ac60c315
StarsSparkle <43267524+StarsSparkle@users.noreply.github.com>
2019-08-27 16:38:51 +0800

    Add files via upload

---
+++ 8.27 _1.py
@@ -0,0 +1,595 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 12:25:12 2019
+
+@author: teng
+"""
+
+def main():
+    str1 = 'hello, world!'
+    # 通过len函数计算字符串的长度
+    print(len(str1))  # 13
+    # 获得字符串首字母大写的拷贝
+    print(str1.capitalize())  # Hello, world!
+    # 获得字符串变大写后的拷贝
+    print(str1.upper())  # HELLO, WORLD!
+    # 从字符串中查找子串所在位置
+    print(str1.find('or'))  # 8
+    print(str1.find('shit'))  # -1
+    # 与find类似但找不到子串时会引发异常
+    # print(str1.index('or'))
+    # print(str1.index('shit'))
+    # 检查字符串是否以指定的字符串开头
+    print(str1.startswith('He'))  # False
+    print(str1.startswith('hel'))  # True
+    # 检查字符串是否以指定的字符串结尾
+    print(str1.endswith('!'))  # True
+    # 将字符串以指定的宽度居中并在两侧填充指定的字符
+    print(str1.center(50, '*'))
+    # 将字符串以指定的宽度靠右放置左侧填充指定的字符
+    print(str1.rjust(50, ' '))
+    str2 = 'abc123456'
+    # 从字符串中取出指定位置的字符(下标运算)
+    print(str2[2])  # c
+    # 字符串切片(从指定的开始索引到指定的结束索引)
+    print(str2[2:5])  # c12
+    print(str2[2:])  # c123456
+    print(str2[2::2])  # c246
+    print(str2[::2])  # ac246
+    print(str2[::-1])  # 654321cba
+    print(str2[-3:-1])  # 45
+    # 检查字符串是否由数字构成
+    print(str2.isdigit())  # False
+    # 检查字符串是否以字母构成
+    print(str2.isalpha())  # False
+    # 检查字符串是否以数字和字母构成
+    print(str2.isalnum())  # True
+    str3 = '  jackfrued@126.com '
+    print(str3)
+    # 获得字符串修剪左右两侧空格的拷贝
+    print(str3.strip())
+
+
+if __name__ == '__main__':
+    main()
+
+
+#定义列表、使用下标访问列表元素以及添加和删除元素的操作。
+def main():
+    list1 = [1, 3, 5, 7, 100]
+    print(list1)
+    list2 = ['hello'] * 5
+    print(list2)
+    # 计算列表长度(元素个数)
+    print(len(list1))
+    # 下标(索引)运算
+    print(list1[0])
+    print(list1[4])
+    # print(list1[5])  # IndexError: list index out of range
+    print(list1[-1])
+    print(list1[-3])
+    list1[2] = 300
+    print(list1)
+    # 添加元素
+    list1.append(200)
+    list1.insert(1, 400)
+    list1 += [1000, 2000]
+    print(list1)
+    print(len(list1))
+    # 删除元素
+    list1.remove(3)
+    if 1234 in list1:
+        list1.remove(1234)
+    del list1[0]
+    print(list1)
+    # 清空列表元素
+    list1.clear()
+    print(list1)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+#通过切片操作我们可以实现对列表的复制或者将列表中的一部分取出来创建出新的列表，代码如下所示。
+def main():
+    fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+    fruits += ['pitaya', 'pear', 'mango']
+    # 循环遍历列表元素
+    for fruit in fruits:
+        print(fruit.title(), end=' ')
+    print()
+    # 列表切片
+    fruits2 = fruits[1:4]
+    print(fruits2)
+    # fruit3 = fruits  # 没有复制列表只创建了新的引用
+    # 可以通过完整切片操作来复制列表
+    fruits3 = fruits[:]
+    print(fruits3)
+    fruits4 = fruits[-3:-1]
+    print(fruits4)
+    # 可以通过反向切片操作来获得倒转后的列表的拷贝
+    fruits5 = fruits[::-1]
+    print(fruits5)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+#对列表的排序操作。
+def main():
+    list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+    list2 = sorted(list1)
+    # sorted函数返回列表排序后的拷贝不会修改传入的列表
+    # 函数的设计就应该像sorted函数一样尽可能不产生副作用
+    list3 = sorted(list1, reverse=True)
+    # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+    list4 = sorted(list1, key=len)
+    print(list1)
+    print(list2)
+    print(list3)
+    print(list4)
+    # 给列表对象发出排序消息直接在列表对象上进行排序
+    list1.sort(reverse=True)
+    print(list1)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+#使用列表的生成式语法来创建列表，代码如下所示。
+import sys
+
+
+def main():
+    f = [x for x in range(1, 10)]
+    print(f)
+    f = [x + y for x in 'ABCDE' for y in '1234567']
+    print(f)
+    # 用列表的生成表达式语法创建列表容器
+    # 用这种语法创建列表之后元素已经准备就绪所以需要耗费较多的内存空间
+    f = [x ** 2 for x in range(1, 10)]
+    print(sys.getsizeof(f))  # 查看对象占用内存的字节数
+    print(f)
+    # 请注意下面的代码创建的不是一个列表而是一个生成器对象
+    # 通过生成器可以获取到数据但它不占用额外的空间存储数据
+    # 每次需要数据的时候就通过内部的运算得到数据(需要花费额外的时间)
+    f = (x ** 2 for x in range(1, 10))
+    print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间
+    print(f)
+    for val in f:
+        print(val)
+
+
+if __name__ == '__main__':
+    main()
+
+
+#yield生成器https://blog.csdn.net/mieleizhi0522/article/details/82142856
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        yield a
+
+
+def main():
+    for val in fib(10):
+        print(val)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+#元组的元素不能修改。
+#顾名思义，我们把多个元素组合到一起就形成了一个元组，所以它和列表一样可以保存多条数据。
+#元组在创建时间和占用的空间上面都优于列表。
+#可以使用sys模块的getsizeof函数来检查存储同样的元素的元组和列表各自占用了多少内存空间
+def main():
+    # 定义元组
+    t = ('骆昊', 38, True, '四川成都')
+    print(t)
+    # 获取元组中的元素
+    print(t[0])
+    print(t[3])
+    # 遍历元组中的值
+    for member in t:
+        print(member)
+    # 重新给元组赋值
+    # t[0] = '王大锤'  # TypeError
+    # 变量t重新引用了新的元组原来的元组将被垃圾回收
+    t = ('王大锤', 20, True, '云南昆明')
+    print(t)
+    # 将元组转换成列表
+    person = list(t)
+    print(person)
+    # 列表是可以修改它的元素的
+    person[0] = '李小龙'
+    person[1] = 25
+    print(person)
+    # 将列表转换成元组
+    fruits_list = ['apple', 'banana', 'orange']
+    fruits_tuple = tuple(fruits_list)
+    print(fruits_tuple)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+def main():
+    set1 = {1, 2, 3, 3, 3, 2}
+    print(set1)
+    print('Length =', len(set1))
+    set2 = set(range(1, 10))
+    print(set2)
+    set1.add(4)
+    set1.add(5)
+    set2.update([11, 12])
+    print(set1)
+    print(set2)
+    set2.discard(5)
+    # remove的元素如果不存在会引发KeyError
+    if 4 in set2:
+        set2.remove(4)
+    print(set2)
+    # 遍历集合容器
+    for elem in set2:
+        print(elem ** 2, end=' ')
+    print()
+    # 将元组转换成集合
+    set3 = set((1, 2, 3, 3, 2, 1))
+    print(set3.pop())
+    print(set3)
+    # 集合的交集、并集、差集、对称差运算
+    print(set1 & set2)
+    # print(set1.intersection(set2))
+    print(set1 | set2)
+    # print(set1.union(set2))
+    print(set1 - set2)
+    # print(set1.difference(set2))
+    print(set1 ^ set2)
+    # print(set1.symmetric_difference(set2))
+    # 判断子集和超集
+    print(set2 <= set1)
+    # print(set2.issubset(set1))
+    print(set3 <= set1)
+    # print(set3.issubset(set1))
+    print(set1 >= set2)
+    # print(set1.issuperset(set2))
+    print(set1 >= set3)
+    # print(set1.issuperset(set3))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+典
+def main():
+    scores = {'骆昊': 95, '白元芳': 78, '狄仁杰': 82}
+    # 通过键可以获取字典中对应的值
+    print(scores['骆昊'])
+    print(scores['狄仁杰'])
+    # 对字典进行遍历(遍历的其实是键再通过键取对应的值)
+    for elem in scores:
+        print('%s\t--->\t%d' % (elem, scores[elem]))
+    # 更新字典中的元素
+    scores['白元芳'] = 65
+    scores['诸葛王朗'] = 71
+    scores.update(冷面=67, 方启鹤=85)
+    print(scores)
+    if '武则天' in scores:
+        print(scores['武则天'])
+    print(scores.get('武则天'))
+    # get方法也是通过键获取对应的值但是可以设置默认值
+    print(scores.get('武则天', 60))
+    # 删除字典中的元素
+    print(scores.popitem())
+    print(scores.popitem())
+    print(scores.pop('骆昊', 100))
+    # 清空字典
+    scores.clear()
+    print(scores)
+
+
+if __name__ == '__main__':
+    main()
+
+
+#练习1：在屏幕上显示跑马灯文字
+import os
+import time
+
+
+def main():
+    content = '北京欢迎你为你开天辟地…………'
+    while True:
+        # 清理屏幕上的输出
+        os.system('cls')  # os.system('clear')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+#练习2：设计一个函数产生指定长度的验证码，验证码由大小写字母和数字构成。
+import random
+
+
+def generate_code(code_len=4):
+    """
+    生成指定长度的验证码
+
+    :param code_len: 验证码的长度(默认4个字符)
+
+    :return: 由大小写英文字母和数字构成的随机验证码
+    """
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0, last_pos)
+        code += all_chars[index]
+    return code
+
+if __name__=='__main__':
+    mycode=generate_code(5)
+    print(mycode)
+
+
+#rfind() 返回字符串最后一次出现的索引，如果没有匹配项则返回-1
+#find() 返回字符串第一次出现的索引，如果没有匹配项则返回-1
+#练习3：设计一个函数返回给定文件名的后缀名。
+def get_suffix(filename, has_dot=False):
+    """
+    获取文件名的后缀名
+
+    :param filename: 文件名
+    :param has_dot: 返回的后缀名是否需要带点
+    :return: 文件的后缀名
+    """
+    pos = filename.rfind('.')
+    if 0 < pos < len(filename) - 1:
+        index = pos if has_dot else pos + 1
+        return filename[index:]
+    else:
+        return ''
+
+if __name__=='__main__':
+    mycode=get_suffix('my.docx')
+    print(mycode)
+
+#练习4：设计一个函数返回传入的列表中最大和第二大的元素的值。
+def max2(a):
+    a=sorted(a)
+    return a[-2],a[-1]
+
+if __name__=='__main__':
+     
+     print(max2([1,3,44,22,45,14]))
+    
+
+
+#计算指定的年月日是这一年的第几天
+def is_leap_year(year):
+    """
+    判断指定的年份是不是闰年
+
+    :param year: 年份
+    :return: 闰年返回True平年返回False
+    """
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def which_day(year, month, date):
+    """
+    计算传入的日期是这一年的第几天
+
+    :param year: 年
+    :param month: 月
+    :param date: 日
+    :return: 第几天
+    """
+    days_of_month = [
+        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ][is_leap_year(year)]
+    total = 0
+    for index in range(month - 1):
+        total += days_of_month[index]
+    return total + date
+
+
+def main():
+    print(which_day(1980, 11, 28))
+    print(which_day(1981, 12, 31))
+    print(which_day(2018, 1, 1))
+    print(which_day(2016, 3, 1))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+def YangHui (num = 10):
+    '''
+    LL = [[1]]
+    for i in range(1,num):
+        LL.append([(0 if j== 0 else LL[i-1][j-1])+ (0 if j ==len(LL[i-1]) else LL[i-1][j]) for j in range(i+1)])
+    return LL
+    '''
+    LL = [[1]]
+    for i in range (1,num):
+        L=[]
+        for j in range(i+1):
+            if j==0:
+                pre=0
+            else:
+                pre=LL[i-1][j-1]
+            if j==i:
+                end=0
+            else:
+                end=LL[i-1][j]
+            L.append(pre+end)
+        LL.append(L)
+    return LL
+        
+
+if __name__ == '__main__':
+    
+    print(YangHui())
+
+
+# =============================================================================
+# #双色球选号
+# from random import randrange, randint, sample
+# 
+# 
+# def display(balls):
+#     """
+#     输出列表中的双色球号码
+#     """
+#     for index, ball in enumerate(balls):
+#         if index == len(balls) - 1:
+#             print('|', end=' ')
+#         print('%02d' % ball, end=' ')
+#     print()
+# 
+# 
+# def random_select():
+#     """
+#     随机选择一组号码
+#     """
+#     red_balls = [x for x in range(1, 34)]
+#     selected_balls = []
+#     #sample(序列a，n).功能：从序列a中随机抽取n个元素，并将n个元素生以list形式返回。
+#     selected_balls = sample(red_balls, 6)
+#     selected_balls.sort()
+#     selected_balls.append(randint(1, 16))
+#     return selected_balls
+# 
+# 
+# def main():
+#     n = int(input('机选几注: '))
+#     for _ in range(n):
+#         display(random_select())
+# 
+# 
+# if __name__ == '__main__':
+#     main()
+# 
+# =============================================================================
+
+
+"""
+《幸运的基督徒》
+有15个基督徒和15个非基督徒在海上遇险，为了能让一部分人活下来不得不将其中15个人扔到海里面去，有个人想了个办法就是大家围成一个圈，由某个人开始从1报数，报到9的人就扔到海里面，他后面的人接着从1开始报数，报到9的人继续扔到海里面，直到扔掉15个人。由于上帝的保佑，15个基督徒都幸免于难，问这些人最开始是怎么站的，哪些位置是基督徒哪些位置是非基督徒。
+"""
+
+def dropone():
+    persons = [True] * 30
+    count=0
+    num=0
+    index=0
+    while count<15:
+        num+=1
+        if num==9:
+            num=0
+            count=+1
+            persons[index]=False
+        index+=1
+        index %= 30
+        
+    for person in persons:
+        print('基' if person else '非', end='')
+        
+def main():
+    persons = [True] * 30
+    counter, index, number = 0, 0, 0
+    while counter < 15:
+        if persons[index]:
+            number += 1
+            if number == 9:
+                persons[index] = False
+                counter += 1
+                number = 0
+        index += 1
+        index %= 30
+    for person in persons:
+        print('基' if person else '非', end='')
+
+
+if __name__ == '__main__':
+    main()
+
+
+import os
+
+
+def print_board(board):
+    print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
+    print('-+-+-')
+    print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
+    print('-+-+-')
+    print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
+
+
+def main():
+    init_board = {
+        'TL': ' ', 'TM': ' ', 'TR': ' ',
+        'ML': ' ', 'MM': ' ', 'MR': ' ',
+        'BL': ' ', 'BM': ' ', 'BR': ' '
+    }
+    begin = True
+    while begin:
+        curr_board = init_board.copy()
+        begin = False
+        turn = 'x'
+        counter = 0
+        os.system('clear')
+        print_board(curr_board)
+        while counter < 9:
+            move = input('轮到%s走棋, 请输入位置: ' % turn)
+            if curr_board[move] == ' ':
+                counter += 1
+                curr_board[move] = turn
+                if turn == 'x':
+                    turn = 'o'
+                else:
+                    turn = 'x'
+            os.system('clear')
+            print_board(curr_board)
+        choice = input('再玩一局?(yes|no)')
+        begin = choice == 'yes'
+
+
+if __name__ == '__main__':
+    main() 
+
+
+
+
+
+
+
+
+
